376693df0c2ff3626c2e8d372a2ecadbcc564137
Rafael Vargas <vargas718@gmail.com>
2018-12-16 18:58:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,8 +20,10 @@ function sayHiToGrandma(string){
 }
   if(string.toUpperCase()===string){
   return `YES INDEED!`
+  }
    if(string ===`I love you, Grandma.`){
   return `I love you, too.`
+}
 }
     
   
